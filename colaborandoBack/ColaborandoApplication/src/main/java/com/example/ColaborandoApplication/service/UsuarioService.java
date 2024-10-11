4e205e2ae6c000e2db0d8119f5a6e8f5518eaccf
gavilan.lucas4 <gavilan.lucas4@gmail.com>
2024-10-11 11:11:11 -0300

    Se comenta linea en usuarioservices

--- colaborandoBack/ColaborandoApplication/src/main/java/com/example/ColaborandoApplication/service/UsuarioService.java
+++ colaborandoBack/ColaborandoApplication/src/main/java/com/example/ColaborandoApplication/service/UsuarioService.java
@@ -66,7 +66,7 @@ public class UsuarioService {
             user.setEmail(email);
             user.setPassword(password);
             user.setTipoUsuario(tipoPersona);
-            user.setFechaCreacion(new Date());
+            /*user.setFechaCreacion(new Date());*/
         return user;
     }
 }
