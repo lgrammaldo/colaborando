23b5df82bda4ed62a34dfefd2a8078ab24e4fb7b
adobrowski <alandobrowski@gmail.com>
2024-11-19 07:57:28 -0300

    agregado de busqueda de eventos

---
+++ colaborandoBack/ColaborandoApplication/src/main/java/com/example/ColaborandoApplication/DTO/EventosAllDTO.java
@@ -0,0 +1,23 @@
+package com.example.ColaborandoApplication.DTO;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import java.sql.Date;
+import java.util.Map;
+
+@Data
+@RequiredArgsConstructor
+public class EventosAllDTO {
+    private Integer id_evento;
+    private String nombre;
+    private Date fecha_publicacion;
+    private Date fecha_fin_busqueda;
+    private Date fecha_inicio;
+    private Date fecha_fin;
+    private Integer userId;
+    private String especificaciones;
+    private String descripcion;
+    private Map<Integer, Integer> empleosYcantidad;
+
+}
